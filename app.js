60531e9e37af4f5d0dd0863cd8503b485106222f
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-07-31 16:09:08 +0600

    learning javascript

---
+++ app.js
@@ -0,0 +1,45 @@
+//====================MATH START====================//
+// let num = -6;
+// let result = 4.21;
+// let myArr = [22, 44, 66, 88, 99, 77, 55, 33, 11];
+// console.log(Math.abs(num));
+// console.log(Math.ceil(result));
+// console.log(Math.floor(result));
+// console.log(Math.max(22, 44, 66, 88, 99, 77, 55, 33, 11));
+// console.log(Math.min(22, 44, 66, 88, 99, 77, 55, 33, 11));
+// console.log(Math.random());
+// console.log(Math.round(result));
+//====================MATH ENDED====================//
+
+//====================JAVASCRIPT NUMBER START====================//
+// let num = 6;
+// let result = 4.21;
+// console.log(Number.isFinite(result));
+// console.log(Number.isInteger(num));
+// console.log(Number.parseFloat(result));
+// console.log(Number.parseInt(result));
+// console.log(result.toFixed(1));
+// console.log(result.toString());
+//====================JAVASCRIPT NUMBER ENDED====================//
+
+//====================WINDOW OBJECT START====================//
+// let myName = "AbuMdNayeemKhan";
+// setTimeout(function(){
+//     console.log(myName)
+// }, 3000); 
+//====================WINDOW OBJECT ENDED====================//
+
+//====================NAVIGATOR OBJECT START====================//
+// navigator.geolocation.getCurrentPosition(function(position){
+//     let altitude = position.coords.altitude;
+//     let latitude = position.coords.latitude;
+//     let longitude = position.coords.longitude;
+//     let speed = position.coords.speed;
+    
+//     document.write(latitude);
+// });
+//====================NAVIGATOR OBJECT ENDED====================//
+
+//====================DOM START====================//
+
+//====================DOM ENDED====================//
